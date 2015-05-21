0aa452280fec7c9fccf2b61241eba736c8136358
luctogno <luctogno@yahoo.it>
2015-05-21 17:16:54 +0200

    corretti scope e aggiunti controller

--- js/angularExamples.js
+++ js/angularExamples.js
@@ -1,11 +1,23 @@
-/*jshint strict:false */
+/* jshint strict:false */
 /* global angular */
 
-	var module = angular.module('PresentationModule', []);
-    
-    module.controller('Controller', ['$scope', function($scope){
-		'use strict';
-        $scope.modello = '';
-        
-        $scope.
-    }]);
+var module = angular.module('PresentationModule', []);
+
+module.controller('Controller', ['$scope', function($scope){
+    'use strict';
+    $scope.modello = '';
+}]);
+
+module.controller('shapeController', function($scope) {
+    $scope.message = 'In shape controller';
+    $scope.type = 'Shape';
+});
+
+module.controller('circleController', function($scope) {
+    $scope.message = 'In circle controller';
+});
+
+module.controller('squareController', function($scope) {
+    $scope.message = 'In square controller';
+    $scope.type = 'Square';
+});
